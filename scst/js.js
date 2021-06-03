f228244009683022b0720a2f6b70a99c3671e708
32ryh98yfe <84429810+32ryh98yfe@users.noreply.github.com>
2021-06-03 16:43:15 +0900

    Add files via upload

---
+++ scst/js.js
@@ -0,0 +1,21 @@
+function update(e){
+    var x = e.clientX || e.touches[0].clientX
+    var y = e.clientY || e.touches[0].clientY
+  
+    document.documentElement.style.setProperty('--cursorX', x + 'px')
+    document.documentElement.style.setProperty('--cursorY', y + 'px')
+  }
+  
+  document.addEventListener('mousemove',update)
+  document.addEventListener('touchmove',update)
+
+
+
+
+
+  
+  function hzplay($mp3){
+    hz = document.getElementById("hz");
+    hz.src = $mp3;
+    hz.play();
+}
